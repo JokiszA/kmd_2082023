fbc251ba6945be63b2127677fd915f4365dba55d
Grzegorz Piwowarek <gpiwowarek@gmail.com>
2023-08-03 14:24:42 +0200

    14. Tasks

--- rental-store/src/main/java/com/pivovarit/Application.java
+++ rental-store/src/main/java/com/pivovarit/Application.java
@@ -3,6 +3,11 @@ package com.pivovarit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/*
+1. Stworzyli nowy serwis "movie-descriptions"
+2. HTTP GET /movie-descriptions/{id} i ma zwracać JSON {"description": "lorem ipsum"}
+3. Wszystkie endpointy rental-store'a mają zwracać movie rozszerzony o opis
+ */
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
