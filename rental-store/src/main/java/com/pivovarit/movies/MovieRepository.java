83498437531437c6a872ebb05e6447c1f7797dd7
Grzegorz Piwowarek <gpiwowarek@gmail.com>
2023-08-03 11:17:09 +0200

    Comments

--- rental-store/src/main/java/com/pivovarit/movies/MovieRepository.java
+++ rental-store/src/main/java/com/pivovarit/movies/MovieRepository.java
@@ -3,6 +3,7 @@ package com.pivovarit.movies;
 import java.util.List;
 import java.util.Optional;
 
+// port (hexagonal)
 interface MovieRepository {
     List<Movie> findAll();
     List<Movie> findAllByType(MovieType type);
