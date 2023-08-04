5d26e87ea1d2101a3675d6a5fb02aa884107b7d9
Grzegorz Piwowarek <gpiwowarek@gmail.com>
2023-08-04 18:15:06 +0200

    Remove instant

--- rental-store/src/main/java/com/pivovarit/rental/api/event/RentalEvent.java
+++ rental-store/src/main/java/com/pivovarit/rental/api/event/RentalEvent.java
@@ -2,7 +2,7 @@ package com.pivovarit.rental.api.event;
 
 import java.time.Instant;
 
-public record RentalEvent(Instant timestamp, RentalEventType eventType, String accountId, String movieTitle) {
+public record RentalEvent(RentalEventType eventType, String accountId, String movieTitle) {
 
     public enum RentalEventType {
         RENT, RETURN
