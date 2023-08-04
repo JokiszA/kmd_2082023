489bb88e98902e81aed6df39703542bf9bbb8d4e
Grzegorz Piwowarek <gpiwowarek@gmail.com>
2023-08-04 11:01:13 +0200

    18. Event sourcing

--- rental-store/src/main/java/com/pivovarit/Application.java
+++ rental-store/src/main/java/com/pivovarit/Application.java
@@ -4,6 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+/**
+ * EVENT SOURCING
+ *
+ * 1. HTTP POST /movies/rent { "accountId": "foo", "title":"Spiderman" }
+ * 2. HTTP POST /movies/return { "accountId": "foo", "title":"Spiderman" }
+ * 3. Zapisujemy w tabeli movie-rental-history, która ma kolumny: id, event type, account id, title
+ * Potem: nie można wypożyczyć więcej niż 10 filmów
+ *
+ */
 @EnableScheduling
 @SpringBootApplication
 public class Application {
