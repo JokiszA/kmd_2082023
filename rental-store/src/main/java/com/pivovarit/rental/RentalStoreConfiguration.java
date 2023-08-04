220980900505017d05de59f22dcbb4a2627b6184
Grzegorz Piwowarek <gpiwowarek@gmail.com>
2023-08-04 13:42:07 +0200

    22. Postgres-based rental history

--- rental-store/src/main/java/com/pivovarit/rental/RentalStoreConfiguration.java
+++ rental-store/src/main/java/com/pivovarit/rental/RentalStoreConfiguration.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
 import org.springframework.jdbc.core.JdbcTemplate;
 
@@ -41,6 +42,12 @@ class RentalStoreConfiguration {
     @Bean
     public RentalMonitor rentalMonitor(RentalRepository rentalRepository) {
         return new RentalMonitor(rentalRepository);
+    }
+
+    @Bean
+    @Profile("default")
+    public RentalRepository inmemoryRentalRepository(JdbcTemplate jdbcTemplate) {
+        return new JdbcRentalRepository(jdbcTemplate);
     }
 
     /*
